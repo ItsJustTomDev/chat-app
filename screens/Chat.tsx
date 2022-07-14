f591af53fb3caf5cbd36870268d3aa6843e42303
ItsJustTomDev <96871598+ItsJustTomDev@users.noreply.github.com>
2022-07-14 17:41:46 +0300

    added offline functionallity

--- screens/Chat.tsx
+++ screens/Chat.tsx
@@ -3,23 +3,33 @@ import { onAuthStateChanged, signInAnonymously, User } from "firebase/auth";
 import { addDoc, collection, onSnapshot, DocumentData, QuerySnapshot, orderBy, query } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 
-import { KeyboardAvoidingView, Platform, StyleSheet, Text, View } from "react-native";
-import { GiftedChat } from "react-native-gifted-chat";
+import { StyleSheet, View } from "react-native";
+import { GiftedChat, InputToolbar } from "react-native-gifted-chat";
 import { auth, db } from "../firebase/firebase-config";
 import { RootStackParmList } from "../navigation/RootStack";
 
 import { IMessage } from "./types";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import NetInfo from "@react-native-community/netinfo";
 
 type Props = NativeStackScreenProps<RootStackParmList, "Chat">;
 
 const Chat = ({ navigation, route }: Props) => {
   const [messages, setMessages] = useState<IMessage[]>([]);
+  const [hasInternet, setHasInternet] = useState<boolean>(true);
   const [user, setUser] = useState<User>();
   const { backgroundColor, username } = route.params;
 
   useEffect(() => {
     navigation.setOptions({
       title: username,
+    });
+
+    NetInfo.fetch().then((connection) => {
+      if (!connection.isConnected) {
+        setHasInternet(false);
+        getMessages();
+      }
     });
 
     const authUnsubscribe = onAuthStateChanged(auth, (user) => {
@@ -67,11 +77,38 @@ const Chat = ({ navigation, route }: Props) => {
         name: username,
       },
     });
+
+    saveMessages();
+  };
+
+  const getMessages = async () => {
+    let messages: any = "";
+    try {
+      messages = (await AsyncStorage.getItem("messages")) || [];
+      setMessages(JSON.parse(messages));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const saveMessages = async () => {
+    try {
+      await AsyncStorage.setItem("messages", JSON.stringify(messages));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const renderInputToolbar = (props: any) => {
+    if (!hasInternet) {
+    } else {
+      return <InputToolbar {...props} />;
+    }
   };
 
   return (
     <View style={{ ...styles.container, backgroundColor: backgroundColor }}>
-      <GiftedChat messages={messages} onSend={(message: IMessage[]) => addMessage(message)} />
+      <GiftedChat messages={messages} onSend={(message: IMessage[]) => addMessage(message)} renderInputToolbar={renderInputToolbar} />
     </View>
   );
 };
