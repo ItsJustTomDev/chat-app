0ec202c5ab44af1e2ac7c624ce7fc07c6b086f25
ItsJustTomDev <96871598+ItsJustTomDev@users.noreply.github.com>
2022-07-14 17:15:35 +0300

    Added firebase!

---
+++ firebase/firebase-config.ts
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD2gVxntwj7PXye3tXUFpD9gDmTBEkG7Cg",
+  authDomain: "chat-app-3294e.firebaseapp.com",
+  projectId: "chat-app-3294e",
+  storageBucket: "chat-app-3294e.appspot.com",
+  messagingSenderId: "961878983577",
+  appId: "1:961878983577:web:f3c1b11e6b7c78f4c20908",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db };
--- screens/Chat.tsx
+++ screens/Chat.tsx
@@ -1,28 +1,20 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { onAuthStateChanged, signInAnonymously, User } from "firebase/auth";
+import { addDoc, collection, onSnapshot, DocumentData, QuerySnapshot, orderBy, query } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 
 import { KeyboardAvoidingView, Platform, StyleSheet, Text, View } from "react-native";
-import { GiftedChat, User } from "react-native-gifted-chat";
+import { GiftedChat } from "react-native-gifted-chat";
+import { auth, db } from "../firebase/firebase-config";
 import { RootStackParmList } from "../navigation/RootStack";
+
+import { IMessage } from "./types";
 
 type Props = NativeStackScreenProps<RootStackParmList, "Chat">;
 
-export interface IMessage {
-  _id: string | number;
-  text: string;
-  createdAt: Date | number;
-  user: User;
-  image?: string;
-  video?: string;
-  audio?: string;
-  system?: boolean;
-  sent?: boolean;
-  received?: boolean;
-  pending?: boolean;
-}
-
 const Chat = ({ navigation, route }: Props) => {
   const [messages, setMessages] = useState<IMessage[]>([]);
+  const [user, setUser] = useState<User>();
   const { backgroundColor, username } = route.params;
 
   useEffect(() => {
@@ -30,41 +22,56 @@ const Chat = ({ navigation, route }: Props) => {
       title: username,
     });
 
-    setMessages([
-      {
-        _id: 3,
-        text: "Hello world!",
-        createdAt: new Date(),
-        user: {
-          _id: 2,
-          name: username,
-        },
-      },
-      {
-        _id: 2,
-        text: "Its a beautiful day today!",
-        createdAt: new Date(),
-        user: {
-          _id: 3,
-          name: username,
-        },
-      },
-      {
-        _id: 1,
-        text: `Welcome to the chat ${username}`,
-        createdAt: new Date(),
-        user: {
-          _id: 2,
-          name: username,
-        },
-        system: true,
-      },
-    ]);
+    const authUnsubscribe = onAuthStateChanged(auth, (user) => {
+      if (!user) {
+        signInAnonymously(auth);
+      } else {
+        setUser(user);
+      }
+    });
+
+    return authUnsubscribe;
   }, []);
+
+  useEffect(() => {
+    const messagesCollectionRef = collection(db, "Messages");
+    const q = query(messagesCollectionRef, orderBy("createdAt", "desc"));
+    const unsubscribe = onSnapshot(q, onCollectionUpdate);
+
+    return unsubscribe;
+  }, []);
+
+  const onCollectionUpdate = (querySnapshot: QuerySnapshot) => {
+    const messages2: IMessage[] = [];
+    querySnapshot.forEach((doc: DocumentData) => {
+      let data = doc.data();
+      messages2.push({
+        _id: data._id,
+        text: data.text,
+        createdAt: data.createdAt.toDate(),
+        user: data.user,
+      });
+    });
+    setMessages(messages2);
+  };
+
+  const addMessage = async (message: IMessage[]) => {
+    const messagesCollectionRef = collection(db, "Messages");
+
+    await addDoc(messagesCollectionRef, {
+      text: message[0].text,
+      _id: message[0]._id,
+      createdAt: message[0].createdAt,
+      user: {
+        _id: user?.uid,
+        name: username,
+      },
+    });
+  };
 
   return (
     <View style={{ ...styles.container, backgroundColor: backgroundColor }}>
-      <GiftedChat messages={messages} />
+      <GiftedChat messages={messages} onSend={(message: IMessage[]) => addMessage(message)} />
     </View>
   );
 };
